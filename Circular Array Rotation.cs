978f1ffd4e2a13608b82f4fae66189d082abc3d2
Rafał <44783572+rniedzielak@users.noreply.github.com>
2018-12-27 12:31:22 +0100

    Create Circular Array Rotation.cs

---
+++ Circular Array Rotation.cs
@@ -0,0 +1,60 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the circularArrayRotation function below.
+    static int[] circularArrayRotation(int[] a, int k, int[] queries)
+    {
+        int[] helping_array = new int[a.Length];
+        for (int i = 0; i < a.Length; i++)
+        {
+            helping_array[(i + k) % a.Length] = a[i]; //all in this simple solution...
+        }
+        for (int i = 0; i < queries.Length; i++)
+        {
+            queries[i] = helping_array[queries[i]];
+        }
+        return queries;
+    }
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string[] nkq = Console.ReadLine().Split(' ');
+
+        int n = Convert.ToInt32(nkq[0]);
+
+        int k = Convert.ToInt32(nkq[1]);
+
+        int q = Convert.ToInt32(nkq[2]);
+
+        int[] a = Array.ConvertAll(Console.ReadLine().Split(' '), aTemp => Convert.ToInt32(aTemp))
+        ;
+
+        int[] queries = new int [q];
+
+        for (int i = 0; i < q; i++) {
+            int queriesItem = Convert.ToInt32(Console.ReadLine());
+            queries[i] = queriesItem;
+        }
+
+        int[] result = circularArrayRotation(a, k, queries);
+
+        textWriter.WriteLine(string.Join("\n", result));
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
