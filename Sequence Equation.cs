d0eebfb4102d82408b31e6d56af75829b39721fd
Rafał <44783572+rniedzielak@users.noreply.github.com>
2018-12-27 12:32:51 +0100

    Create Sequence Equation.cs

---
+++ Sequence Equation.cs
@@ -0,0 +1,44 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the permutationEquation function below.
+    static int[] permutationEquation(int[] p)
+    {
+        int[] result = new int[p.Length];
+        for (int i = 0; i < p.Length; i++)
+        {
+            int value = Array.IndexOf(p, Array.IndexOf(p, i + 1) + 1) + 1;
+            
+            result[i] = value;
+        }
+        return result;
+    }
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] p = Array.ConvertAll(Console.ReadLine().Split(' '), pTemp => Convert.ToInt32(pTemp))
+        ;
+        int[] result = permutationEquation(p);
+
+        textWriter.WriteLine(string.Join("\n", result));
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
