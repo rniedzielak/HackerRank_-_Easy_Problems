448c7a5a5fdce09ac59a7905f161864a6f6ae89f
Rafał <44783572+rniedzielak@users.noreply.github.com>
2018-12-27 11:50:37 +0100

    Create Divisible Sum Pairs.cs

---
+++ Divisible Sum Pairs.cs
@@ -0,0 +1,53 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the divisibleSumPairs function below.
+    static int divisibleSumPairs(int n, int k, int[] ar)
+    {
+        List<int> result = new List<int>();
+        for(int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                int sum = ar[i]+ar[j];
+                if(j != i && sum % k == 0)
+                {
+                    result.Add(sum);
+                }
+            }
+        }
+        return result.Count / 2;
+    }
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string[] nk = Console.ReadLine().Split(' ');
+
+        int n = Convert.ToInt32(nk[0]);
+
+        int k = Convert.ToInt32(nk[1]);
+
+        int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp))
+        ;
+        int result = divisibleSumPairs(n, k, ar);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
