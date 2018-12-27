082b4e72197326a479c4a5f8066088438f41b3ab
Rafał <44783572+rniedzielak@users.noreply.github.com>
2018-12-27 11:46:46 +0100

    Create Time Conversion.cs

---
+++ Time Conversion.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class Solution {
+
+    /*
+     * Complete the timeConversion function below.
+     */
+    static string timeConversion(string s)
+    {
+        DateTime dt = DateTime.Parse(s);
+        string hour_24_format = dt.ToString("HH:mm:ss");
+        return hour_24_format;
+    }
+
+    static void Main(string[] args) {
+        TextWriter tw = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string s = Console.ReadLine();
+
+        string result = timeConversion(s);
+
+        tw.WriteLine(result);
+
+        tw.Flush();
+        tw.Close();
+    }
+}
