c02c2296566bf07e3b39b033e094b8092cda4d71
Rafał <44783572+rniedzielak@users.noreply.github.com>
2018-12-27 13:03:06 +0100

    Create Alternating Characters.cs

---
+++ Alternating Characters.cs
@@ -0,0 +1,47 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the alternatingCharacters function below.
+    static int alternatingCharacters(string s)
+    {
+        int counter = 0;
+        for(int i = 1; i< s.Length; i++)
+        {
+            if(s[i]==s[i-1])
+            {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int q = Convert.ToInt32(Console.ReadLine());
+
+        for (int qItr = 0; qItr < q; qItr++) {
+            string s = Console.ReadLine();
+
+            int result = alternatingCharacters(s);
+
+            textWriter.WriteLine(result);
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
