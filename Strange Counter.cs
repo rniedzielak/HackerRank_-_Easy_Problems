f38f0d3ee9e742e906f143414edbdbdaa1755da7
Rafał <44783572+rniedzielak@users.noreply.github.com>
2018-12-29 12:39:45 +0100

    Update Strange Counter.cs
    This is next mathematic riddle. To solve it we must read something about geometric sequence.

--- Strange Counter.cs
+++ Strange Counter.cs
@@ -12,9 +12,8 @@ using System.Text.RegularExpressions;
 using System.Text;
 using System;
 
-class Solution {
-
-    // Complete the strangeCounter function below.
+class Solution
+{    // Complete the strangeCounter function below.
     static long strangeCounter(long t)
     {//I will use the nomenclature in mathematics of geometric series:
         long sequence = (long)Math.Ceiling(Math.Log((double)t / 3 + 1, 2));
@@ -26,19 +25,14 @@ class Solution {
         long goBack = t - (3 * ((long)q_n1 - 1)); /* t if be sum of sequence
                                                     then always answear be 1*/
         return an - (goBack - 1);
-
-
     }
 
-    static void Main(string[] args) {
+    static void Main(string[] args)
+    {
         TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
-
         long t = Convert.ToInt64(Console.ReadLine());
-
         long result = strangeCounter(t);
-
         textWriter.WriteLine(result);
-
         textWriter.Flush();
         textWriter.Close();
     }
