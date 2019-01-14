26d75ac8333dfeb946fc691fd53e9192e98dbbf1
Rafał <44783572+rniedzielak@users.noreply.github.com>
2019-01-14 07:39:42 +0100

    Create Lonely Integer.cs

---
+++ Lonely Integer.cs
@@ -0,0 +1,44 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the lonelyinteger function below.
+    static int lonelyinteger(int[] a)
+    {
+       for(int i = 0; i<a.Length; i++)
+       {
+           if(a.Count(v=>v==a[i])==1)
+           {
+               return a[i];
+           }
+       }
+       return -1;
+    }
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] a = Array.ConvertAll(Console.ReadLine().Split(' '), aTemp => Convert.ToInt32(aTemp))
+        ;
+        int result = lonelyinteger(a);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
