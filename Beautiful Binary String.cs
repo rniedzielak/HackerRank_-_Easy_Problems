11a5a2f1911830a1e02a090a3a9c87a862500a09
Rafał <44783572+rniedzielak@users.noreply.github.com>
2019-01-17 07:33:02 +0100

    Create Beautiful Binary String.cs

---
+++ Beautiful Binary String.cs
@@ -0,0 +1,37 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the beautifulBinaryString function below.
+    static int beautifulBinaryString(string b)
+    {
+        return ((b.Length - b.Replace("010", "").Length)/3); // One-lineer
+    }
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        string b = Console.ReadLine();
+
+        int result = beautifulBinaryString(b);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
