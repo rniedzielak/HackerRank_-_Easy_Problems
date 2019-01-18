b6f26ef5ff51ca9067a594cfc623ba779b04761a
Rafał <44783572+rniedzielak@users.noreply.github.com>
2019-01-18 16:55:47 +0100

    Create Marc's Cakewalk.cs

---
+++ Marc's Cakewalk.cs
@@ -0,0 +1,38 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+
+    // Complete the marcsCakewalk function below.
+    static long marcsCakewalk(int[] calorie)
+    {
+        return calorie.OrderByDescending(x => x).Select((t, i) => (long)(t * Math.Pow(2, i))).Sum();
+    }
+
+    static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] calorie = Array.ConvertAll(Console.ReadLine().Split(' '), calorieTemp => Convert.ToInt32(calorieTemp));
+        long result = marcsCakewalk(calorie);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
