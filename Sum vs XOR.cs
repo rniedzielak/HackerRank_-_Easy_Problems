70ea1fe728af6dc3b262c607fc8d35c6a011fe04
Rafał <44783572+rniedzielak@users.noreply.github.com>
2019-02-07 06:05:16 +0100

    Create Sum vs XOR.cs

---
+++ Sum vs XOR.cs
@@ -0,0 +1,38 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+    // Complete the sumXor function below.
+    static long sumXor(long n)
+    {
+        if(n==0)
+            return 1;
+        else
+        {
+            string binary = Convert.ToString(n, 2);
+            int numOfZeros = binary.Count(v=> v=='0');
+            return (long)Math.Pow(2, numOfZeros);
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+        long n = Convert.ToInt64(Console.ReadLine().Trim());
+        long result = sumXor(n);
+        textWriter.WriteLine(result);
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
